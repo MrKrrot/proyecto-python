e1c4a27510ac7e3c585521ae8e2a6fe5730b5568
John Doe <elizz.dl25@gmail.com>
2022-11-13 01:44:39 -0600

    Funcion Multiplicar editada

--- mul.py
+++ mul.py
@@ -1,6 +1,6 @@
 def multiplicar():
     cantidad = 1
-    n=int(input("Cantidad de numeros que va a editar: "))
+    n=int(input("Cantidad de numeros que va a multiplicar: "))
     for i in range(n):
         valor = int(input("Numero: "))
         cantidad *= valor 
